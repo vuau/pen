17678d342fdea4746201295202012f47b4086550
Au <au@tinypulse.com>
2020-03-29 22:19:28 +0700

    confirm to delete

--- src/ListItem.svelte
+++ src/ListItem.svelte
@@ -6,6 +6,12 @@
     push(`/notes/${id}`);
   }
 
+  function confirmToDelete(id) {
+    if (confirm('Are you sure to delete this note?')) {
+      deleteNote({ id });
+    }
+  }
+
   export let id = null;
   export let title = '';
 </script>
@@ -14,7 +20,7 @@
   class="hover-to-show pointer dim flex items-center justify-between lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30">
   <span>{title}</span>
   <span
-    on:click={() => deleteNote({ id })}
+    on:click|stopPropagation={() => confirmToDelete(id)}
     class="hover-element-to-show material-icons w2 pointer"
   >delete_outline</span>
 </li>
