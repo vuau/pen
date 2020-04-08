712e400019d065cd1a1181b94a3d88ff11705bd3
Au <au@tinypulse.com>
2020-04-09 00:07:29 +0700

    fix bundle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import 'tachyons'
 import 'material-design-icons/iconfont/material-icons.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'simplemirror/dist/SimpleMirror.css'
-import './style.scss'
+import './style.css'
 import App from './App.svelte'
 
 const app = new App({
