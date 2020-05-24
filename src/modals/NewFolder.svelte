b2f779f037065c728257fa758f1f60f07eeedc9b
Au <phamvuau@gmail.com>
2020-05-24 15:05:00 +0700

    auto focus input

--- src/modals/NewFolder.svelte
+++ src/modals/NewFolder.svelte
@@ -1,9 +1,17 @@
 <script>
   import { notes, modal } from '../stores.js'
+  import { onMount } from 'svelte'
   import { SEA } from '../contexts.js'
   import { whenEnter } from '../utils.js'
 
   let folderName = ''
+  let inputEl
+
+  onMount(() => {
+    if (inputEl) {
+      inputEl.focus()
+    }
+  })
 
   async function onSubmit () {
     await notes.createFolder(folderName, $modal.parentId)
@@ -11,11 +19,17 @@
   }
 </script>
 
-<form class="black-80">
+<div class="black-80">
   <div class="measure">
     <label for="folder-name" class="f6 b db mb2">Folder name</label>
-    <input type="text" bind:value={folderName} on:keyup={whenEnter(onSubmit)} id="folder-name" class="input-reset ba
-    b--black-20 pa2 mb2 db w-100" aria-describedby="folder-name">
+    <input
+      bind:this={inputEl}
+      bind:value={folderName}
+      on:keyup={whenEnter(onSubmit)}
+      type="text"
+      id="folder-name"
+      class="input-reset ba b--black-20 pa2 mb2 db w-100"
+      aria-describedby="folder-name">
   </div>
   <div class="mt3">
     <a
@@ -25,4 +39,4 @@
       Save
     </a>
   </div>
-</form>
+</div>
