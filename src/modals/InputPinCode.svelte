bf3a5e0026fb07eee4f8daf7fe54bd1c9087e3ba
Au <au@tinypulse.com>
2020-07-23 15:58:39 +0700

    improve pin code dialog

--- src/modals/InputPinCode.svelte
+++ src/modals/InputPinCode.svelte
@@ -4,7 +4,7 @@
   import { user, modal } from '../stores.js'
   import { debounce, whenEnter } from '../utils.js'
 
-  let pin
+  let pin = ''
   let inputEl
   let isProcessing
 
@@ -20,36 +20,37 @@
     $modal.onClose()
   }
 
+  function chooseNumber (number) {
+    return () => {
+      pin = `${pin}${number}` 
+      checkSubmit(pin)
+    }
+  }
+
   function onLogin () {
     localStorage.removeItem('auth')
     location.reload()
   }
-
-  onMount(() => {
-    if (inputEl) inputEl.focus()
-  })
 </script>
 
 <form on:submit|preventDefault class="black-80">
   <div class="measure">
     <label for="pin" class="f6 b db mb2">Unlock by pin code</label>
-    <input type="number" bind:this={inputEl} bind:value={pin} on:keyup={debounce(checkSubmit, 100)} id="pin" class="input-reset ba b--black-20 pa2 mb2 db w-100"
-    aria-describedby="pin-desc" />
     <small id="pin-desc" class="f6 black-60 db mb2">
       You can find your Pin code in the account setting
     </small>
+    <input type="password" value={pin} class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+    <div class="numpad ba flex flex-wrap">
+      {#each Array.from(Array(10), (_, i) => i >= 9 ? 0 : i + 1) as number}
+        <div class="ph2 pv3 w-third tc pointer" on:click={chooseNumber(number)}>{number}</div>
+      {/each}
+    </div>
   </div>
   <div class="mt3">
     <a
       href="#0"
-      on:click|preventDefault={checkSubmit}
-      class="f6 link dim br1 ph3 pv2 mb2 dib white bg-black">
-      Verify
-    </a>
-    <a
-      href="#0"
       on:click|preventDefault={onLogin}
-      class="f6 link dim br1 ph2 pv2 mb2 dib black">
+      class="f6 link dim br1 ph3 pv2 mb2 dib white bg-black">
       or Login with password
     </a>
   </div>
