20eb5a3d5fb084d3dc3f3e990a5969357cad63ce
Au <au@tinypulse.com>
2020-09-08 08:33:46 +0700

    add more relay peers

--- src/contexts.js
+++ src/contexts.js
@@ -4,13 +4,26 @@ import 'gun/lib/not'
 import 'gun/lib/then'
 import 'gun/lib/webrtc'
 
-let gun
+let peers
 
 if (process.env.NODE_ENV === 'production') {
-  gun = Gun(['https://pensync.glitch.me/gun'])
+  peers = [
+    // https://github.com/amark/gun/wiki/volunteer.dht
+    'https://www.raygun.live/gun',
+    'https://gunmeetingserver.herokuapp.com/gun',
+    'https://gun-us.herokuapp.com/gun',
+    'https://gun-eu.herokuapp.com/gun',
+    // My own relay peer
+    'https://pensync.glitch.me/gun',
+    'https://pvaklb.ddns.net/gun'
+  ]
 } else {
-  gun = Gun(['http://localhost:8765/gun'])
+  peers = ['http://localhost:8765/gun']
 }
+
+const gun = new Gun({
+  peers
+})
 
 const gunUser = gun.user()
 
