d80836d76e5a118ad54fc8e6395ce26264ec1835
Au <au@tinypulse.com>
2020-09-14 08:31:11 +0700

    update peers

--- src/contexts.js
+++ src/contexts.js
@@ -6,16 +6,7 @@ import 'gun/lib/then'
 let peers
 
 if (process.env.NODE_ENV === 'production') {
-  peers = [
-    // https://github.com/amark/gun/wiki/volunteer.dht
-    'https://www.raygun.live/gun',
-    'https://gunmeetingserver.herokuapp.com/gun',
-    'https://gun-us.herokuapp.com/gun',
-    'https://gun-eu.herokuapp.com/gun',
-    // My own relay peer
-    'https://pensync.glitch.me/gun',
-    'https://pvaklb.ddns.net/gun'
-  ]
+  peers = ['https://pensync.glitch.me/gun']
 } else {
   peers = ['http://localhost:8765/gun']
 }
