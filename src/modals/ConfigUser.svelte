026be79f63584c8546559d96c86b6dee8f66c1fe
Au <au@tinypulse.com>
2020-09-18 21:11:42 +0700

    backup to json file

--- src/modals/ConfigUser.svelte
+++ src/modals/ConfigUser.svelte
@@ -1,21 +1,94 @@
 <script>
-  import { user, modal } from '../stores.js'
-  import { SEA } from '../contexts.js'
+  import FileSaver from 'file-saver'
+  import { user, modal, decrypt, getParentNode } from '../stores.js'
   import { whenEnter } from '../utils.js'
 
   let pin = $user.config ? $user.config.pin : ''
+  let searchTimeout
+  let isSearching = false
+  let startBackup = false
+  let searchResults = {}
 
   async function onSubmit () {
     await user.updateConfig({ pin })
     $modal.onClose()
+  }
+
+  function doSearch (text, path) {
+    searchTimeout = setTimeout(() => {
+      isSearching = false
+    }, 1000)
+    const node = getParentNode(path)
+    node.map().once(async (data, id) => {
+      if (!data) return
+      const decryptedData = await decrypt(data)
+      if (decryptedData.type === 'folder') {
+        const newPath = [
+          ...(path || '').split('_').filter(p => p !== ''),
+          id
+        ].join('_')
+        doSearch(text, newPath)
+      } else {
+        if (
+          (decryptedData.title &&
+            decryptedData.title.toLowerCase().includes(text.toLowerCase())) ||
+          (decryptedData.content &&
+            decryptedData.content.toLowerCase().includes(text.toLowerCase()))
+        ) {
+          searchResults = {
+            ...searchResults,
+            [id]: {
+              id,
+              ...decryptedData,
+              path
+            }
+          }
+          clearTimeout(searchTimeout)
+        }
+      }
+    })
+  }
+  function backup () {
+    searchResults = {}
+    startBackup = true
+    isSearching = true
+    doSearch('')
+  }
+  $: {
+    if (startBackup && !isSearching) {
+      // DONE
+      console.log(searchResults)
+      const blob = new Blob([JSON.stringify(searchResults, null, 2)], {
+        type: 'application/json'
+      })
+      const date = new Date()
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1 // "+ 1" becouse the 1st month is 0
+      const day = date.getDate()
+      const hour = date.getHours()
+      const minutes = date.getMinutes()
+      const filename = `backup-${month}.${day}.${year}-${hour}:${minutes}.json`
+      FileSaver.saveAs(blob, filename)
+      startBackup = false
+    }
   }
 </script>
 
 <form class="black-80">
   <div class="measure">
     <label for="pin" class="f6 b db mb2">Pin Code</label>
-    <input type="number" bind:value={pin} on:keyup={whenEnter(onSubmit)} id="pin" class="input-reset ba b--black-20 pa2 mb2 db w-100" aria-describedby="pin-desc">
-    <small id="pin-desc" class="f6 black-60 db mb2">Pin is used to unlock your session.</small>
+    <input
+      type="number"
+      bind:value={pin}
+      on:keyup={whenEnter(onSubmit)}
+      id="pin"
+      class="input-reset ba b--black-20 pa2 mb2 db w-100"
+      aria-describedby="pin-desc" />
+    <small id="pin-desc" class="f6 black-60 db mb2">Pin is used to unlock your
+      session.</small>
+    <button on:click|preventDefault={backup}>
+      {#if isSearching}Processing...{:else}Backup{/if}
+    </button>
   </div>
   <div class="mt3">
     <a
