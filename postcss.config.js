09a9c25cc25327b54e27a4a076903c394c06d707
Au <au@tinypulse.com>
2020-09-26 21:07:15 +0700

    minify css

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const purgeCss = require('@fullhuman/postcss-purgecss')
 const autoprefixer = require('autoprefixer')
 const postcssNested = require('postcss-nested')
 const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
 
 const purgeConfig = {
   css: ['./src/styles/main.css', './src/styles/water.css'],
@@ -11,7 +12,7 @@ const purgeConfig = {
 
 const plugins =
   process.env.NODE_ENV === 'production'
-    ? [postcssNested, autoprefixer, postcssPresetEnv, purgeCss(purgeConfig)]
+    ? [postcssNested, autoprefixer, postcssPresetEnv, purgeCss(purgeConfig), cssnano()]
     : [postcssNested, autoprefixer, postcssPresetEnv]
 
 module.exports = { plugins }
